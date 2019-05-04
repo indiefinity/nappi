a389b13b473659aa3019514f359473863f6b5daf
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-04 18:45:40 +0300

    a

--- public/AWS/nappi.js
+++ public/AWS/nappi.js
@@ -1,8 +1,5 @@
 const { nappi } = process.env;
 
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: nappi
-    };
+    document.getElementById('nappi').innerHTML = nappi
 };
