c944a4c7c31375fc3ed2fe3f4265587d2acb0804
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-05 13:08:36 +0300

    3w

--- public/tatu.js
+++ public/tatu.js
@@ -1,1 +1,6 @@
-console.log("ooth")
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+function login() {
+    b1.style = "display: none"
+    b2.style = "display: block"
+}
