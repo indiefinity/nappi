abd6cd94bfc22905483dd34540e2d25d2f71cdd8
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-05 13:12:31 +0300

    AH AH AH

--- public/tatu.js
+++ public/tatu.js
@@ -1,6 +1,6 @@
 var b1 = document.getElementById("b1");
 var b2 = document.getElementById("b2");
 function login() {
-    b1.style = "display: none"
-    b2.style = "display: block"
+    b1.style.display = "none"
+    b2.style.display = "block"
 }
