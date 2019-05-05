39f350fd4c1b81324d2e027b11d25bb448614edd
viluzi <ville@kastell.tk>
2019-05-05 13:21:42 +0300

    asd

--- public/tatu.js
+++ public/tatu.js
@@ -5,3 +5,7 @@ function login() {
     document.getElementById("b1").style.display = "none"
     document.getElementById("b2").style.display = "block"
 }
+function login2() {
+    document.getElementById("b1").style.display = "block"
+    document.getElementById("b2").style.display = "none"
+}
