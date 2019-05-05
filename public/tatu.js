0737ab33ba1ca42e22c8d32186f04ce1b26c980e
viluzi <ville@kastell.tk>
2019-05-05 13:18:59 +0300

    Update tatu.js

--- public/tatu.js
+++ public/tatu.js
@@ -1,5 +1,6 @@
 var b1 = document.getElementById("b1");
 var b2 = document.getElementById("b2");
+console.log(b1, b2)
 function login() {
     b1.style.display = "none"
     b2.style.display = "block"
